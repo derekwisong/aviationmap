754c678871b45c869f878b29d793a540a90eda51
Derek Wisong <derekwisong@gmail.com>
2019-07-07 09:36:24 -0400

    Lock WS2801 controller resources for thread safety

--- ledvfrmap/rpi.py
+++ ledvfrmap/rpi.py
@@ -7,30 +7,49 @@ import logging
 class LEDController(threading.Thread):
     def __init__(self, pixel_count, clock_pin, data_pin):
         threading.Thread.__init__(self)
-        self.pixels = Adafruit_WS2801.WS2801Pixels(pixel_count, clk=clock_pin,
+        self.pixels = Adafruit_WS2801.WS2801Pixels(pixel_count,
+                                                   clk=clock_pin,
                                                    do=data_pin)
         self.all_off()
         self.changed = False
         self.stopped = threading.Event()
+        self.lock = threading.Lock()
 
     def stop(self):
         self.stopped.set()
 
     def all_off(self):
-        self.pixels.clear()
-        self.pixels.show()
-    
+        self.lock.acquire()
+        try:
+            self.pixels.clear()
+            self.pixels.show()
+        finally:
+            self.lock.release()
+
     def set_rgb(self, pixel_number, r, g, b):
-        self.pixels.set_pixel_rgb(pixel_number, r, g, b)
-        self.changed = True
+        self.lock.acquire()
+        try:
+            current = self.get_rgb(pixel_number)
+
+            if current != (r, g, b):
+                self.pixels.set_pixel_rgb(pixel_number, r, g, b)
+                self.changed = True
+        finally:
+            self.lock.release()
     
     def get_rgb(self, pixel_number):
         return self.pixels.get_pixel_rgb(pixel_number)
-
+        
     def run(self):
         logger = logging.getLogger(__name__)
-        while not self.stopped.wait(0.5):
+        while not self.stopped.wait(0.1):
             if self.changed:
                 logger.info("LED color(s) changed, showing the new color(s)")
-                self.pixels.show()
-                self.changed = False
+                self.lock.acquire()
+                try:
+                    self.pixels.show()
+                finally:
+                    self.lock.release()
+                    self.changed = False
+
+        self.all_off()
